10a2b6214907af37b3fb97b8b1c425bf2b4d034d
Guilherme Freitas (Software Developer) <139295562+GuilhermeSK2@users.noreply.github.com>
2024-10-22 01:20:57 -0300

    First Commit

---
+++ products/products/src/main/java/com/freitas/products/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.freitas.products.repositories;
+
+
+import com.freitas.products.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
